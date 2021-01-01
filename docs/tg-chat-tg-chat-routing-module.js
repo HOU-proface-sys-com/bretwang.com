d82b69d4972e80b7d6cb78bebd58374c1f759409
王　朋 <76829092+HOU-proface-sys-com@users.noreply.github.com>
2021-01-01 17:47:41 +0900

    Add files via upload

---
+++ docs/tg-chat-tg-chat-routing-module.js
@@ -0,0 +1,39 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["tg-chat-tg-chat-routing-module"],{
+
+/***/ "12EG":
+/*!***************************************************!*\
+  !*** ./src/app/tg-chat/tg-chat-routing.module.ts ***!
+  \***************************************************/
+/*! exports provided: TgChatRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TgChatRoutingModule", function() { return TgChatRoutingModule; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/router */ "tyNb");
+/* harmony import */ var _tg_chat_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./tg-chat.component */ "4TCD");
+
+
+
+
+
+const routes = [{ path: '', component: _tg_chat_component__WEBPACK_IMPORTED_MODULE_2__["TgChatComponent"] }];
+class TgChatRoutingModule {
+}
+TgChatRoutingModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineNgModule"]({ type: TgChatRoutingModule });
+TgChatRoutingModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineInjector"]({ factory: function TgChatRoutingModule_Factory(t) { return new (t || TgChatRoutingModule)(); }, imports: [[_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"].forChild(routes)], _angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵsetNgModuleScope"](TgChatRoutingModule, { imports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]], exports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]] }); })();
+/*@__PURE__*/ (function () { _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](TgChatRoutingModule, [{
+        type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"],
+        args: [{
+                imports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"].forChild(routes)],
+                exports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]]
+            }]
+    }], null, null); })();
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=tg-chat-tg-chat-routing-module.js.map
---
+++ docs/tg-portal-tg-portal-routing-module.js
@@ -0,0 +1,39 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["tg-portal-tg-portal-routing-module"],{
+
+/***/ "j7LK":
+/*!*******************************************************!*\
+  !*** ./src/app/tg-portal/tg-portal-routing.module.ts ***!
+  \*******************************************************/
+/*! exports provided: TgPortalRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TgPortalRoutingModule", function() { return TgPortalRoutingModule; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/router */ "tyNb");
+/* harmony import */ var _tg_portal_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./tg-portal.component */ "ki56");
+
+
+
+
+
+const routes = [{ path: '', component: _tg_portal_component__WEBPACK_IMPORTED_MODULE_2__["TgPortalComponent"] }];
+class TgPortalRoutingModule {
+}
+TgPortalRoutingModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineNgModule"]({ type: TgPortalRoutingModule });
+TgPortalRoutingModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineInjector"]({ factory: function TgPortalRoutingModule_Factory(t) { return new (t || TgPortalRoutingModule)(); }, imports: [[_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"].forChild(routes)], _angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵsetNgModuleScope"](TgPortalRoutingModule, { imports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]], exports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]] }); })();
+/*@__PURE__*/ (function () { _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](TgPortalRoutingModule, [{
+        type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"],
+        args: [{
+                imports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"].forChild(routes)],
+                exports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]]
+            }]
+    }], null, null); })();
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=tg-portal-tg-portal-routing-module.js.map
